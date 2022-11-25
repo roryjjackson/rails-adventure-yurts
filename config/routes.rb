21b17750301321e1f7c7c10e5af50659f7c80db7
rory jackson <roryjamesjackson@hotmail.com>
2022-11-25 10:41:45 +0000

    add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'about', to: "pages#about"
+  get 'contact', to: "pages#contact"
+  get 'product', to: "pages#product"
   # Defines the root path route ("/")
   # root "articles#index"
 end
